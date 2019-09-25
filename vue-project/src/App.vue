6c13f43303b72189073668afd93f86427c778589
Dmitriy <mylnikov059@gmail.com>
2019-09-26 00:49:21 +0500

    Верстка.
    Время выполнения 2 часа:
    -разворачивание проекта
    -придумывание концепции
    -верстка

---
+++ vue-project/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="app">
+        <page-header />
+        <page-content />
+    </div>
+</template>
+
+<script>
+    import PageHeader from './components/PageHeader.vue'
+    import PageContent from './components/PageContent.vue'
+
+    export default {
+        name: 'app',
+        components: {
+            PageHeader,
+            PageContent
+        }
+    }
+</script>
+
+<style src="../src/assets/sass/app.scss" lang="scss"/>
---
+++ vue-project/src/components/PageHeader.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="header">
+        List issues
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'PageHeader'
+    }
+</script>
+
+<style src="../assets/sass/components/PageHeader.scss" lang="scss"/>
