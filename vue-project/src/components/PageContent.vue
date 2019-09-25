6ad331e2b179d380ee6d1b0b148a35e8e86cfcd5
Dmitriy <mylnikov059@gmail.com>
2019-09-26 00:47:23 +0500

    Верстка.
    Время выполнения 2 часа:
    -разворачивание проекта
    -придумывание концепции
    -верстка

---
+++ vue-project/src/components/PageContent.vue
@@ -0,0 +1,96 @@
+<template>
+    <div class="content">
+        <div class="list-layout">
+            <div class="list-header space">
+                <div class="list-count">
+                    244 Open
+                </div>
+            </div>
+            <div class="list-content space">
+                <div class="list-item">
+                    <div class="list-item__content">
+                        <div class="list-item__link">
+                            <a href="">
+                                componentInstance is undefined with v-slot syntax on this.$slots[slots] VNodes
+                            </a>
+                            <a href="">
+                                <span class="tag">
+                                    bug
+                                </span>
+                            </a>
+                        </div>
+                        <div class="list-item__small-text">
+                            #10554 opened yesterday by petr001
+                        </div>
+                    </div>
+                    <a href="">
+                        <div class="list-item__comments">
+                            <div class="list-item__comments-icon"></div>
+                            <div class="list-item__comments-count">
+                                12
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="list-item">
+                    <div class="list-item__content">
+                        <div class="list-item__link">
+                            <a href="">
+                                componentInstance is undefined with v-slot syntax on this.$slots[slots] VNodes
+                            </a>
+                            <a href="">
+                                <span class="tag">
+                                    bug
+                                </span>
+                            </a>
+                        </div>
+                        <div class="list-item__small-text">
+                            #10554 opened yesterday by petr001
+                        </div>
+                    </div>
+                    <a href="">
+                        <div class="list-item__comments">
+                            <div class="list-item__comments-icon"></div>
+                            <div class="list-item__comments-count">
+                                12
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="list-item">
+                    <div class="list-item__content">
+                        <div class="list-item__link">
+                            <a href="">
+                                componentInstance is undefined with v-slot syntax on this.$slots[slots] VNodes
+                            </a>
+                            <a href="">
+                                <span class="tag">
+                                    bug
+                                </span>
+                            </a>
+                        </div>
+                        <div class="list-item__small-text">
+                            #10554 opened yesterday by petr001
+                        </div>
+                    </div>
+                    <a href="">
+                        <div class="list-item__comments">
+                            <div class="list-item__comments-icon"></div>
+                            <div class="list-item__comments-count">
+                                12
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'PageContent'
+    }
+</script>
+
+<style src="../assets/sass/components/PageContent.scss" lang="scss"/>
